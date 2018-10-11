ed41aea0b8fc1f6e6dd8f6194ceb9bef08fd10a4
hristo2612 <hristoapps@gmail.com>
2018-10-11 22:27:29 +0300

    Have a quick dive with figlet and clear libs

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const clear = require('clear');
 const figlet = require('figlet');
 
 const files = require('./lib/files');
+
+clear();
+console.log(
+    chalk.yellow(
+        figlet.textSync('Git Init', { horizontalLayout: 'full' })
+    )
+);
